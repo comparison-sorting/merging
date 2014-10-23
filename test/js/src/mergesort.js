d28d668203712d0b7de553961b2bf47b1dec913e
aureooms <aurelien.ooms@gmail.com>
2014-10-23 17:03:07 +0200

    style

--- test/js/src/mergesort.js
+++ test/js/src/mergesort.js
@@ -1,61 +1,73 @@
-var util, array, random, operator;
+var all, util, array, random, operator, functools, itertools, shuffle;
 
 util = require( "util" );
 array = require( "aureooms-js-array" );
 random = require( "aureooms-js-random" );
 operator = require( "aureooms-js-operator" );
+functools = require( "aureooms-js-functools" );
+itertools = require( "aureooms-js-itertools" );
 
-var check = function(ctor, n, diff) {
-	var name = util.format("mergesort (new %s(%d), %s)", ctor.name, n, diff);
-	console.log(name);
-	test(name, function (assert) {
+shuffle = random.__shuffle__( random.__sample__( random.randint ) );
 
-		// SETUP RANDOM
-		var randint = random.randint;
-		var sample = random.__sample__(randint);
-		var shuffle = random.__shuffle__(sample);
-		var iota = array.iota;
-		var copy = array.copy;
+all = function( diffname, diff, mergesortname, mergesort, n, type ) {
 
-		// SETUP SORT
-		var tapemerge = sort.tapemerge;
-		var mergesort = sort.__mergesort__( tapemerge, copy);
+	var title;
+
+	title = util.format( "%s (new %s(%d), %s)", mergesortname, type.name, n, diffname );
+
+	console.log( title );
+
+	test( title, function () {
+
+		var a, d, i, sorted;
 
 		// SETUP ARRAY, DEST
-		var a = new ctor(n);
-		var d = new ctor(n);
-		iota(a, 0, n, 0);
+		a = new type( n );
+		d = new type( n );
+		array.iota( a, 0, n, 0 );
 
 		// SORT ARRAY
-		shuffle(a, 0, n);
-		mergesort( diff, a, 0, n, d, 0, n);
+		shuffle( a, 0, n );
+		mergesort( diff, a, 0, n, d, 0, n );
 
 		// TEST PREDICATE
-		var i = d.length;
-		var sorted = true;
-		if(i > 1){
+		i = d.length;
+		sorted = true;
+
+		if ( i > 1 ) {
+
 			while (--i) {
+
 				if ( diff( d[i-1], d[i] ) > 0 ) {
 					sorted = false;
 					break;
 				}
+
 			}
+
 		}
 
-		ok(sorted, 'check sorted');
-		deepEqual(a.length, n, 'check length a');
-		deepEqual(d.length, n, 'check length d');
+		ok( sorted, "check sorted" );
+		deepEqual( a.length, n, "check length a" );
+		deepEqual( d.length, n, "check length d" );
+
 	});
 };
 
-var DIFF = [
-	sort.increasing,
-	sort.decreasing
-];
+itertools.product( [
 
-var N = [0, 1, 2, 10, 63, 64, 65];
+[
+	[ "increasing", sort.increasing ],
+	[ "decreasing", sort.decreasing ]
+],
 
-var CTOR = [
+[
+	[ "mergesort", sort.__mergesort__( sort.tapemerge, array.copy ) ]
+],
+
+[0, 1, 2, 10, 63, 64, 65],
+
+[
 	Array,
 	Int8Array,
 	Uint8Array,
@@ -65,17 +77,20 @@ var CTOR = [
 	Uint32Array,
 	Float32Array,
 	Float64Array
-];
+]
 
-for (var k = 0; k < CTOR.length; k++) {
-	for (var j = 0; j < N.length; j++) {
-		if(CTOR[k].BYTES_PER_ELEMENT &&
-			N[j] > Math.pow(2, CTOR[k].BYTES_PER_ELEMENT * 8)){
-				continue;
-		}
-		for (var i = 0; i < DIFF.length; ++i) {
-			check(CTOR[k], N[j], DIFF[i]);
-		}
-	}
-}
+], 1, [] ).forEach(
 
+	functools.partial( functools.star,
+
+		function ( diffname, diff, mergesortname, mergesort, n, type ) {
+
+			if ( type.BYTES_PER_ELEMENT && n > Math.pow( 2, type.BYTES_PER_ELEMENT * 8 ) ) {
+				return;
+			}
+
+			all( diffname, diff, mergesortname, mergesort, n, type );
+		}
+	)
+
+);
