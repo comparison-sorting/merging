6adaefaea729a2d37c45613c3e4ed81dfab3137a
aureooms <aurelien.ooms@gmail.com>
2014-10-23 16:39:43 +0200

    update to new signatures + style

--- test/js/src/merge.js
+++ test/js/src/merge.js
@@ -1,4 +1,4 @@
-var util, array, search, random, operator, functools;
+var all, util, array, search, random, operator, functools, itertools;
 
 util = require( "util" );
 array = require( "aureooms-js-array" );
@@ -6,86 +6,69 @@ search = require( "aureooms-js-search" );
 random = require( "aureooms-js-random" );
 operator = require( "aureooms-js-operator" );
 functools = require( "aureooms-js-functools" );
+itertools = require( "aureooms-js-itertools" );
 
-var check = function(tmpl, ctor, m, n, diff) {
-	var name = util.format("%s (new %s(%d, %d), %s)", tmpl[0], ctor.name, m, n, diff);
-	console.log(name);
-	test(name, function (assert) {
+all = function ( diffname, diff, mergename, merge, m, n, type ) {
 
-		// SETUP RANDOM
-		var randint = random.randint;
-		var sample = random.__sample__( randint );
-		var shuffle = random.__shuffle__( sample );
+	var title;
 
-		// SETUP UTILS
-		var copy = array.copy;
+	title = util.format( "%s (new %s(%d, %d), %s)", mergename, type.name, m, n, diffname );
 
-		// SETUP SORT
-		var partition = sort.partition;
-		var quicksort = sort.__quicksort__( partition );
-		var binarysearch = search.binarysearch;
-		var merge = tmpl[1]( diff, binarysearch, copy );
+	console.log( title );
 
-		// SETUP ARRAYS, DEST
-		var a, j;
+	test( title, function () {
 
-		a = new ctor( m );
+		var a, b, c, d, j;
+
+		a = new type( m );
 
 		for ( j = 0 ; j < m ; ++j ) {
-			a[j] = randint( 0, m );
+			a[j] = random.randint( 0, m );
 		}
 
-		shuffle(a, 0, m);
-		quicksort( diff, a, 0, m);
+		array.sort( diff, a );
 
-		var b = new ctor(n);
-		for(j = 0; j < n; ++j) b[j] = randint(0, n);
-		shuffle(b, 0, n);
-		quicksort( diff, b, 0, n);
+		b = new type(n);
+		for ( j = 0 ; j < n ; ++j ) {
+			b[j] = random.randint( 0, n );
+		}
+		array.sort( diff, b );
 
-		var d = new ctor(n + m);
+		d = new type( n + m );
 
 		// MERGE ARRAYS
-		merge(a, 0, m, b, 0, n, d, 0);
+		merge( diff, a, 0, m, b, 0, n, d, 0 );
 
 		// REF
-		var c = new ctor(n + m);
-		copy(a, 0, m, c, 0);
-		copy(b, 0, n, c, m);
-		shuffle(c, 0, n + m);
-		quicksort( diff, c, 0, n + m);
+		c = new type( n + m );
+		array.copy( a, 0, m, c, 0 );
+		array.copy( b, 0, n, c, m );
+		array.sort( diff, c );
 
-		deepEqual(d, c, 'check sorted');
-		deepEqual(a.length, m, 'check length a');
-		deepEqual(b.length, n, 'check length b');
-		deepEqual(d.length, n + m, 'check length d');
+		deepEqual( d, c, "check sorted" );
+		deepEqual( a.length, m, "check length a" );
+		deepEqual( b.length, n, "check length b" );
+		deepEqual( d.length, n + m, "check length d" );
 	});
 };
 
-var DIFF = [
-	sort.increasing,
-	sort.decreasing
-];
+itertools.product([
 
-var TMPL = [
-	['merge', function ( diff, binarysearch, copy ) {
-		var index;
+[
+	[ "increasing", sort.increasing ],
+	[ "decreasing", sort.decreasing ]
+],
 
-		index = functools.partial( binarysearch, [diff] );
+[
+	[ "merge" , sort.__merge__( search.binarysearch, array.copy ) ],
+	[ "binarymerge" , sort.__binarymerge__( search.binarysearch, array.copy ) ],
+	[ "tapemerge" , sort.tapemerge ]
+],
 
-		return sort.__merge__( index, copy );
-	}],
-	['binarymerge', function ( diff, binarysearch, copy ) {
-		return sort.__binarymerge__( binarysearch, diff, copy );
-	}],
-	['tapemerge', function ( diff, binarysearch, copy ) {
-		return functools.partial( sort.tapemerge, [diff] );
-	}],
-];
+[0, 1, 2, 10, 63, 64, 65], // MUST BE IN INCREASING ORDER !!
+[0, 1, 2, 10, 63, 64, 65], // MUST BE IN INCREASING ORDER !!
 
-var N = [0, 1, 2, 10, 63, 64, 65]; // MUST BE IN INCREASING ORDER !!
-
-var CTOR = [
+[
 	Array,
 	Int8Array,
 	Uint8Array,
@@ -95,20 +78,24 @@ var CTOR = [
 	Uint32Array,
 	Float32Array,
 	Float64Array
-];
+]
 
-for(var t = 0; t < TMPL.length; ++t){
-	for (var k = 0; k < CTOR.length; ++k) {
-		for (var j = 0; j < N.length; ++j) {
-			for (var l = 0; l <= j; ++l) {
-				if(CTOR[k].BYTES_PER_ELEMENT && N[j] > Math.pow(2, CTOR[k].BYTES_PER_ELEMENT * 8))
-					continue;
+], 1, [] ).forEach(
 
-				for (var i = 0; i < DIFF.length; ++i) {
-					check(TMPL[t], CTOR[k], N[j], N[l], DIFF[i]);
-				}
+	functools.partial( functools.star,
+
+		function ( diffname, diff, mergename, merge, m, n, type ) {
+
+			if ( type.BYTES_PER_ELEMENT && m > Math.pow( 2, type.BYTES_PER_ELEMENT * 8 ) ) {
+				return;
 			}
-		}
-	}
-}
 
+			if ( m < n ) {
+				return;
+			}
+
+			all( diffname, diff, mergename, merge, m, n, type );
+
+		}
+	)
+);
