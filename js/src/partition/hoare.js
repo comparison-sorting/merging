299a6fa759bf56a16b0339bd7b828a3578d12820
aureooms <aurelien.ooms@gmail.com>
2015-02-19 02:33:05 +0100

    style

--- js/src/partition/hoare.js
+++ js/src/partition/hoare.js
@@ -4,55 +4,55 @@
  * HYP : i < j
  */
 
-var hoare = function ( compare, a, i, j ) {
+var hoare = function ( compare , a , i , j ) {
 
-	var x, t, o;
+	var x , t , o ;
 
-	o = i;
-	x = a[o];
+	o = i ;
+	x = a[o] ;
 
 	while ( true ) {
 
 		while ( true ) {
 
-			--j;
+			--j ;
 
 			if ( i >= j ) {
-				a[o] = a[j];
-				a[j] = x;
-				return j;
+				a[o] = a[j] ;
+				a[j] = x ;
+				return j ;
 			}
 
-			else if ( compare( a[j], x ) <= 0 ) {
-				break;
+			else if ( compare( a[j] , x ) <= 0 ) {
+				break ;
 			}
 		}
 
 		while ( true ) {
 
-			++i;
+			++i ;
 
 			if ( i >= j ) {
-				a[o] = a[j];
-				a[j] = x;
-				return j;
+				a[o] = a[j] ;
+				a[j] = x ;
+				return j ;
 			}
 
-			else if ( compare( x, a[i] ) <= 0 ) {
-				break;
+			else if ( compare( x , a[i] ) <= 0 ) {
+				break ;
 			}
 		}
 
 
 		// invariant i < j
 
-		t    = a[i];
-		a[i] = a[j];
-		a[j] = t;
+		t    = a[i] ;
+		a[i] = a[j] ;
+		a[j] =    t ;
 
 	}
 
-};
+} ;
 
-exports.hoare = hoare;
-exports.partition = hoare;
+exports.hoare = hoare ;
+exports.partition = hoare ;
