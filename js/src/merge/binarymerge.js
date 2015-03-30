b508c7ce3b4e71026d26101781bb27ffefbc38e9
aureooms <aurelien.ooms@gmail.com>
2015-03-30 19:33:52 +0200

    commenting Hwang-Lin

--- js/src/merge/binarymerge.js
+++ js/src/merge/binarymerge.js
@@ -1,51 +1,83 @@
 
 
-/**
- * Merges 2 arrays using the Hwang Lin algorithm.
- *
- *   /!\ aj - ai >= bj - bi
- */
+var __binarymerge__ = function ( binarysearch , copy ) {
 
-var __binarymerge__ = function ( binarysearch, copy ) {
+	/**
+	 * Merges 2 arrays using the Hwang Lin algorithm.
+	 *
+	 *   /!\ aj - ai >= bj - bi
+	 */
 
-	var hwanglin = function ( compare, a, ai, aj, b, bi, bj, c, ci ) {
+	var hwanglin = function ( compare , a , ai , aj , b , bi , bj , c , ci ) {
 
-		var o, t, x, q, d, z;
+		var m , n , o , t , g , q , d , z ;
 
-		o = ci - ai - bi;
-		t = ai;
+		o = ci - ai - bi ;
+		t = ai ;
 
-		x = Math.pow( 2, Math.floor( Math.log( ( aj - ai ) / ( bj - bi ) ) / Math.log( 2 ) ) );
+		m = aj - ai ;
+		n = bj - ai ;
 
-		while ( bi < bj && ( ai + x < aj || ( x = aj - ai - 1 ) ) ) {
+		// g is the size of a block
 
-			t = ai;
-			ai = t + x;
+		g = Math.pow( 2 , Math.floor( Math.log( m / n ) / Math.log( 2 ) ) ) ;
+
+		blocks : while ( bi < bj && ( ai + g < aj || ( g = aj - ai - 1 ) ) ) {
+
+			// for each block
+
+			// t is the inner left bound
+			t = ai ;
+
+			// ai is the inner right bound
+			ai = t + g ;
 
 			while ( bi < bj ) {
 
-				if ( compare( b[bi], a[ai] ) >= 0 ) {
-					copy( a, t, ai, c, o + t + bi );
-					break;
+				// we will try to insert b_i in this block
+
+				if ( compare( b[bi] , a[ai] ) >= 0 ) {
+
+					// b_i must be inserted to the left of this block
+					// we copy the block to the output array and continue
+					// with the next block
+					copy( a , t , ai , c , o + t + bi ) ;
+					continue blocks ;
+
 				}
 
-				q = binarysearch( compare, a, t, ai, b[bi] );
-				z = q[0] + q[1];
+				// b_i is inside this block and smaller than a_i
+				// we search its insertion position using a binary search algorithm
+				q = binarysearch( compare , a , t , ai , b[bi] ) ;
 
-				copy( a, t, z, c, o + t + bi );
-				c[o + z + bi] = b[bi];
-				t = z;
-				++bi;
+				// z is the insertion position
+				z = q[0] + q[1] ;
+
+				// copy everything to the left of the insertion position
+				// to the output array
+				copy( a , t , z , c , o + t + bi ) ;
+
+				// copy b_i to its insertion position in the output array
+				c[o + bi + z] = b[bi] ;
+
+				// update the inner left bound of the block
+				t = z ;
+
+				// go to the next b_i
+				++bi ;
+
 			}
+
 		}
 
-		copy( a, t, aj, c, o + t + bi );
-		copy( b, bi, bj, c, o + aj + bi );
+		// remaining elements are copied to the output array
+		copy( a ,  t , aj , c , o +  t + bi ) ;
+		copy( b , bi , bj , c , o + aj + bi ) ;
 
-	};
+	} ;
 
-	return hwanglin;
+	return hwanglin ;
 
-};
+} ;
 
-exports.__binarymerge__ = __binarymerge__;
+exports.__binarymerge__ = __binarymerge__ ;
