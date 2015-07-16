2dc4d82f5e84af8a593311533b9b0b6fbfc2cdf0
aureooms <aurelien.ooms@gmail.com>
2015-07-16 17:13:27 +0000

    simplify code using aureooms-js-sll

--- test/js/src/nodes.js
+++ test/js/src/nodes.js
@@ -1,26 +1,13 @@
 
+var sll = require( "aureooms-js-sll" ) ;
 var compare = require( "aureooms-js-compare" ) ;
+var itertools = require( "aureooms-js-itertools" ) ;
+var functools = require( "aureooms-js-functools" ) ;
 
-var w = function ( list ) {
+var w = sll.list ;
+var r = functools.compose( [ itertools.list , sll.iter ] ) ;
 
-	if ( list.length === 0 ) return null ;
-
-	return {
-		value : list[0] ,
-		next  : w( list.slice( 1 ) )
-	} ;
-
-} ;
-
-var r = function ( head ) {
-
-	if ( head === null ) return [ ] ;
-
-	return [ head.value ].concat( r( head.next ) ) ;
-
-} ;
-
-var one = function ( compare , A , B , expected , text ) {
+var t = function ( compare , A , B , expected , text ) {
 
 	deepEqual(
 		r( merging.nodes( compare , w(A) , A.length , w(B) , B.length ) ) ,
@@ -31,25 +18,25 @@ var one = function ( compare , A , B , expected , text ) {
 
 test( "iterables" , function ( ) {
 
-	one( compare.increasing , [ 1 , 5 ] , [ 2 , 3 , 4 ] ,
+	t( compare.increasing , [ 1 , 5 ] , [ 2 , 3 , 4 ] ,
 		[ 1 , 2 , 3 , 4 , 5 ] ,
 		" 1 2 3 4 5"
 	) ;
 
 
-	one( compare.increasing , [ 1 ] , [ 2 , 3 , 4 ] ,
+	t( compare.increasing , [ 1 ] , [ 2 , 3 , 4 ] ,
 		[ 1 , 2 , 3 , 4 ] ,
 		" 1 2 3 4"
 	) ;
 
 
-	one( compare.increasing , [ 1 , 5 ] , [ ] ,
+	t( compare.increasing , [ 1 , 5 ] , [ ] ,
 		[ 1 , 5 ] ,
 		" 1 5 "
 	) ;
 
 
-	one( compare.increasing , [ ] , [ 2 , 3 , 4 ] ,
+	t( compare.increasing , [ ] , [ 2 , 3 , 4 ] ,
 		[ 2 , 3 , 4 ] ,
 		" 2 3 4 "
 	) ;
